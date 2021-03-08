5c8d4c789cc1fd9ceccca16b14b196eb855edf9f
dhifandiandra <dhifand@gmail.com>
2021-03-08 17:00:30 +0700

    Add alignItems and justifyContent

--- FlexBox.js
+++ FlexBox.js
@@ -4,11 +4,16 @@ import {Text, View} from 'react-native'
 class FlexBox extends Component {
     render(){
         return (
-            <View style={{flexDirection: 'row'}}>
-                <View style={{backgroundColor: '#ee5253', flex:1, height: 100}} />
+            <View style={{
+                flexDirection: 'row',
+                backgroundColor: '#c8d6e5',
+                alignItems: 'center',
+                justifyContent: 'space-between'}}
+                >
+                <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
                 <View style={{backgroundColor: '#feca57', width: 50, height: 100}} />
-                <View style={{backgroundColor: '#1dd1a1', flex: 2, height: 100}} />
-                <View style={{backgroundColor: '#5f27cd', width: 25, height: 100}} />
+                <View style={{backgroundColor: '#1dd1a1', width: 50, height: 150}} />
+                <View style={{backgroundColor: '#5f27cd', width: 50, height: 200}} />
             </View>
         )
     }
